6ed0c748732f11b1f54bf642e3bf680a7a995ddc
Matthew Morgan <matt@dulcetsoftware.com>
2017-11-18 10:00:02 -0500

    Commit working solution

--- railFenceCipher.js
+++ railFenceCipher.js
@@ -1,15 +1,106 @@
-class RailFenceCipher {
-
-  encode(message, rails = 1) {
-    if (message === 'AAABBB') {
-      if (rails === 3) {
-        return 'ABABBA';
-      }
-      return 'ABABAB';
+export default function () {
+  function generatePattern(height, width) {
+    let pattern = Array(height);
+    for (let row = 0; row < height; row++) {
+      pattern[row] = Array(width).fill('');
     }
-    return message;
+    let action = (container, row, col) => container[row][col] = '*';
+    walkAndDo(pattern, action);
+    return pattern;
+  }
+
+  function walkAndDo(container, action){
+    let row = 0;
+    let col = 0;
+    let inc = 1;
+    let width = container[0].length;
+    let height = container.length;
+
+    while (col < width) {
+      if (row >= height || row < 0) {
+        inc *= -1;
+        row += 2 * inc;
+      }
+      action(container, row, col);
+      row += inc;
+      col++;
+    }
+    return container;
+  }
+
+  return {
+    encode: (plainText, numRails=1) => {
+      if (numRails===1){
+        return plainText;
+      }
+      let pattern = generatePattern(numRails, plainText.length);
+      let text = [...plainText];
+      let row = 0;
+      let col = 0;
+      let inc = 1;
+      while (text.length){
+        if (row >= numRails || row < 0) {
+          inc *= -1;
+          row += 2 * inc;
+        }
+        pattern[row][col] = text.shift();
+        row += inc;
+        col++;
+      }
+      let encodedText = '';
+      for (let row of pattern){
+        for (let cell of row){
+          if (cell != '*'){
+            encodedText += cell;
+          }
+        }
+      }
+
+      return encodedText;
+    },
+    decode: (cipherText, numRails=1) => {
+      if (numRails===1){
+        return cipherText;
+      }
+
+      let pattern = generatePattern(numRails, cipherText.length);
+      let text = [...cipherText];
+
+      let row=0;
+      let col=0;
+      let inc=1;
+      while (text.length) {
+        if (row >= numRails || row < 0) {
+          inc *= -1;
+          row += 2 * inc;
+        }
+        if (pattern[row][col]==='*'){
+          pattern[row][col] = text.shift();
+        }
+        col ++;
+        if (col > cipherText.length){
+          row += inc;
+          col = 0;
+        }
+      }
+
+
+      let plainText = '';
+      row = 0;
+      col = 0;
+      inc = 1;
+      while (plainText.length < cipherText.length){
+        if (row >= numRails || row < 0) {
+          inc *= -1;
+          row += 2 * inc;
+        }
+        plainText += pattern[row][col];
+        row += inc;
+        col++;
+      }
+
+      return plainText;
+
+    }
   }
 }
-
-export default RailFenceCipher;
-
--- railFenceCipher.spec.js
+++ railFenceCipher.spec.js
@@ -1,16 +1,19 @@
 import RailFenceCipher from './railFenceCipher';
 
-describe('RailFenceCipher business class can', () => {
-  let cipher;
+let cipher;
 
-  beforeEach(() => {
-    cipher = new RailFenceCipher();
-  });
+beforeEach(() => {
+  cipher = new RailFenceCipher();
 
-  test('return an instance of the Kata class', () => {
+});
+
+xdescribe('RailFenceCipher business class can', () => {
+  test('return an instance of the RailFenceCipher class', () => {
     expect(cipher.constructor.name).toEqual('RailFenceCipher');
   });
+});
 
+describe('RailFenceCipher encode method can', () => {
   test('encode a single rail message', () => {
     const message = 'Four score and seven years ago.';
     expect(cipher.encode(message)).toEqual(message);
@@ -18,11 +21,54 @@ describe('RailFenceCipher business class can', () => {
 
   test('encode a double rail message', () => {
     const message = 'AAABBB';
-    expect(cipher.encode(message, 2)).toEqual('ABABAB');
+    expect(cipher.encode(message, 2)).toEqual('AABABB');
   });
 
   test('encode the same message with three rails', () => {
     const message = 'AAABBB';
     expect(cipher.encode(message, 3)).toEqual('ABABBA');
   });
+
+  test('encode the same message with four rails', () => {
+    const message = 'AAABBB';
+    expect(cipher.encode(message, 4)).toEqual('AABABB');
+  });
+
+  test('encode a different message with two rails', () => {
+    const message = '0123456789';
+    expect(cipher.encode(message, 2)).toEqual('0246813579');
+  });
+
+  test('encode a different message with three rails', () => {
+    const message = '0123456789';
+    expect(cipher.encode(message, 3)).toEqual('0481357926');
+  });
 });
+
+describe('RailFenceCipher decode method can', () => {
+  test('decode a single rail message', () => {
+    const message = 'Four score and seven years ago.';
+    expect(cipher.decode(message)).toEqual(message);
+  });
+
+  test('decode a double rail message', () => {
+    const message = 'AABB';
+    expect(cipher.decode(message, 2)).toEqual('ABAB');
+  });
+
+  test('decode a different double rail message', () => {
+    const message = '1324';
+    expect(cipher.decode(message, 2)).toEqual('1234');
+  });
+
+  test('decode a longer double rail message', () => {
+    const message = '0123456789';
+    expect(cipher.decode(message, 2)).toEqual('0516273849');
+  });
+
+  test('decode a three rail message', () => {
+    const message = '0123456789';
+    expect(cipher.decode(message, 3)).toEqual('0384159627');
+  });
+});
+
