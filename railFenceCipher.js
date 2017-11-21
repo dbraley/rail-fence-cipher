8d574c0d77f69a876d9ed22e6191ef7e4807802b
David Braley <david.braley@gmail.com>
2017-11-21 17:48:18 -0500

    Refactor

--- railFenceCipher.js
+++ railFenceCipher.js
@@ -7,42 +7,42 @@ class RailFenceCipher {
     if (input.length <= this.rails) {
       return input;
     }
-    const rail_0 = [];
-    const rail_1 = [];
-    const rail_2 = [];
-    const rail_3 = [];
+    const myRails = [];
+    for (let i = 0; i < this.rails; i++) {
+      myRails[i] = [];
+    }
     if (this.rails === 2) {
-      rail_0.push(input[0]);
-      rail_1.push(input[1]);
+      myRails[0].push(input[0]);
+      myRails[1].push(input[1]);
       if (input.length >= 3) {
-        rail_0.push(input[2]);
+        myRails[0].push(input[2]);
       }
       if (input.length >= 4) {
-        rail_1.push(input[3]);
+        myRails[1].push(input[3]);
       }
       if (input.length >= 5) {
-        rail_0.push(input[4]);
+        myRails[0].push(input[4]);
       }
     } else if (this.rails === 3) {
-      rail_0.push(input[0]);
-      rail_1.push(input[1]);
-      rail_2.push(input[2]);
+      myRails[0].push(input[0]);
+      myRails[1].push(input[1]);
+      myRails[2].push(input[2]);
       if (input.length >= 4) {
-        rail_1.push(input[3]);
+        myRails[1].push(input[3]);
       }
       if (input.length >= 5) {
-        rail_0.push(input[4]);
+        myRails[0].push(input[4]);
       }
     } else if (this.rails === 4) {
-      rail_0.push(input[0]);
-      rail_1.push(input[1]);
-      rail_2.push(input[2]);
-      rail_3.push(input[3]);
+      myRails[0].push(input[0]);
+      myRails[1].push(input[1]);
+      myRails[2].push(input[2]);
+      myRails[3].push(input[3]);
       if (input.length >= 5) {
-        rail_2.push(input[4]);
+        myRails[2].push(input[4]);
       }
     }
-    return [].concat(rail_0, rail_1, rail_2, rail_3).join('');
+    return [].concat(myRails[0], myRails[1], myRails[2], myRails[3]).join('');
   }
 
 }
