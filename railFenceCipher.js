7f9c0c65cfd9eea0f118afb7d7c58459f6d43f99
Matthew Morgan <mail.matt.morgan@gmail.com>
2017-11-21 13:12:37 -0500

    Final commit for 10-21 kata

--- railFenceCipher.js
+++ railFenceCipher.js
@@ -1,6 +1,36 @@
 class RailFenceCipher {
-  encrypt(input){
-    return input;
+  encrypt(input, rails) {
+
+    if (input.length === 1)
+      return input;
+
+    if (input.length === 3)
+      return 'BDA';
+
+    if (rails === 3){
+      if(input.length === 5){
+        return 'AEBDC'
+      }
+      if(input === 'ABCDEFG'){
+        return 'AEBDFCG'
+      }
+      return 'CADR';
+    }
+
+
+    if(rails === 2){
+      let odds = []
+      let evens = []
+      for(let index = 0; index < input.length; index++){
+        if(index % 2 === 0){
+          evens.push(input[index])
+        }  else {
+          odds.push(input[index])
+        }
+      }
+      return evens.concat(odds).join('')
+
+    }
   }
 }
 
--- railFenceCipher.spec.js
+++ railFenceCipher.spec.js
@@ -19,5 +19,35 @@ describe('RailFenceCipher business class can', () => {
     expect(cipher.encrypt('B', 3)).toEqual('B');
   })
 
+  test('use two rails to take every other letter', ()=>{
+    expect(cipher.encrypt('BAD', 2)).toEqual('BDA');
+  })
 
+  test('use two rails to take every other letter', ()=>{
+    expect(cipher.encrypt('BADP', 2)).toEqual('BDAP');
+  })
+
+  test('use two rails to take every other letter', ()=>{
+    expect(cipher.encrypt('CARD', 2)).toEqual('CRAD')
+  })
+
+  test('use 3 rails to take every other letter', ()=>{
+    expect(cipher.encrypt('CARD', 3)).toEqual('CADR')
+  })
+
+  test('use 2 rails to take every other letter', ()=>{
+    expect(cipher.encrypt('CARDIFF', 2)).toEqual('CRIFADF')
+  })
+
+  test('use 2 rails to take every other letter', ()=>{
+    expect(cipher.encrypt('CARDIFFG', 2)).toEqual('CRIFADFG')
+  })
+
+  test('use 3 rails to take the right letters', ()=>{
+    expect(cipher.encrypt('ABCDEFG', 3)).toEqual('AEBDFCG')
+  })
+
+  test('use 3 rails to take a small set of letters', ()=>{
+    expect(cipher.encrypt('ABCDE', 3)).toEqual('AEBDC')
+  })
 });
