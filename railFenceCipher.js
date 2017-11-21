1394ab7610f8c880689f89af69c51e8355943ce2
David Braley <david.braley@gmail.com>
2017-11-21 17:34:26 -0500

    Refactor

--- railFenceCipher.js
+++ railFenceCipher.js
@@ -13,7 +13,11 @@ class RailFenceCipher {
         rail_0.push(input[2]);
         return [].concat(rail_0, rail_1).join('');
       } else if (input.length === 4) {
-        return input[0] + input[2] + input[1] + input[3];
+        rail_0.push(input[0]);
+        rail_1.push(input[1]);
+        rail_0.push(input[2]);
+        rail_1.push(input[3]);
+        return [].concat(rail_0, rail_1).join('');
       } else if (input.length === 5) {
         return input[0] + input[2] + input[4] + input[1] + input[3];
       }
