07368d9e336a2db5edb56e32944610036a0e9ca7
David Braley <david.braley@gmail.com>
2017-11-21 20:18:32 -0500

    Refactor

--- railFenceCipher.js
+++ railFenceCipher.js
@@ -4,13 +4,7 @@ class RailFenceCipher {
   }
 
   encrypt(input) {
-    if (input.length <= this.rails) {
-      return input;
-    }
-    const myRails = [];
-    for (let i = 0; i < this.rails; i++) {
-      myRails[i] = [];
-    }
+    const myRails = this.initRails();
     if (this.rails === 2) {
       for (let i = 0; i < input.length; i++) {
         let rail = i%2;
@@ -23,17 +17,22 @@ class RailFenceCipher {
         myRails[rail].push(input[i]);
       }
     } else if (this.rails === 4) {
-      myRails[0].push(input[0]);
-      myRails[1].push(input[1]);
-      myRails[2].push(input[2]);
-      myRails[3].push(input[3]);
-      if (input.length >= 5) {
-        myRails[2].push(input[4]);
+      let railToUse = [0,1,2,3,2,1];
+      for (let i = 0; i < input.length; i++) {
+        let rail = railToUse[i%(railToUse.length)];
+        myRails[rail].push(input[i]);
       }
     }
     return [].concat(myRails[0], myRails[1], myRails[2], myRails[3]).join('');
   }
 
+  initRails() {
+    const myRails = [];
+    for (let i = 0; i < this.rails; i++) {
+      myRails[i] = [];
+    }
+    return myRails;
+  }
 }
 
 export default RailFenceCipher;
