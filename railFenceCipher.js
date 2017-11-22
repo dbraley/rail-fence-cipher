f56014fc17294d54d87118b87abd7676e8265557
David Braley <david.braley@gmail.com>
2017-11-21 20:27:33 -0500

    Refactor

--- railFenceCipher.js
+++ railFenceCipher.js
@@ -5,19 +5,24 @@ class RailFenceCipher {
 
   encrypt(input) {
     const myRails = this.initRails();
-    let railToUse;
-    if (this.rails === 2) {
-      railToUse = [0,1];
-    } else if (this.rails === 3) {
-      railToUse = [0,1,2,1];
-    } else if (this.rails === 4) {
-      railToUse = [0,1,2,3,2,1];
-    }
+    let railToUse = this.generateRailToUse();
     for (let i = 0; i < input.length; i++) {
       let rail = railToUse[i%(railToUse.length)];
       myRails[rail].push(input[i]);
     }
     return this.joinRails(myRails);
+  }
+
+  generateRailToUse() {
+    let railToUse;
+    if (this.rails === 2) {
+      railToUse = [0, 1];
+    } else if (this.rails === 3) {
+      railToUse = [0, 1, 2, 1];
+    } else if (this.rails === 4) {
+      railToUse = [0, 1, 2, 3, 2, 1];
+    }
+    return railToUse;
   }
 
   joinRails(myRails) {
