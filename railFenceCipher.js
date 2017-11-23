218287c1b247cf9d21b3d12dec1888fd35ba4a47
David Braley <david.braley@gmail.com>
2017-11-22 19:01:41 -0500

    Refactor to minimize code

--- railFenceCipher.js
+++ railFenceCipher.js
@@ -1,51 +1,51 @@
+// import _ from 'underscore';
 class RailFenceCipher {
   constructor(rails) {
-    this.rails = rails;
+    this.railCount = rails;
+    this.railMapping = this.generateRailMapping();
+  }
+
+  generateRailMapping() {
+    if (this.railCount === undefined || this.railCount <= 1) return [0];
+    return [].concat(range(0, this.railCount - 1)).concat(range(this.railCount - 1, 0, -1));
   }
 
   encrypt(input) {
-    const myRails = this.initRails();
-    let railToUse = this.generateRailToUse();
-    for (let i = 0; i < input.length; i++) {
-      let rail = railToUse[i%(railToUse.length)];
-      myRails[rail].push(input[i]);
-    }
-    return this.joinRails(myRails);
+    const myRails = initArrayOfArrays(this.railCount);
+    input.split('').forEach((letter, index) => {
+      myRails[this.getRailNumberForIndex(index)].push(letter);
+    });
+    return flattenToString(myRails);
   }
 
-  generateRailToUse() {
-    if(this.rails === 1) {
-      return [0];
-    }
-    let railToUse;
-    if (this.rails === 2) {
-      railToUse = [0, 1];
-    } else if (this.rails === 3) {
-      railToUse = [0, 1, 2, 1];
-    } else {
-      railToUse = [];
-      for (let i = 0; i < this.rails - 1; i++) {
-        railToUse.push(i);
-      }
-      for (let i = this.rails - 1; i > 0; i--) {
-        railToUse.push(i);
-      }
-    }
-    return railToUse;
-  }
-
-  joinRails(myRails) {
-    return [].concat(myRails[0], myRails[1], myRails[2], myRails[3]).join('');
-  }
-
-  initRails() {
-    const myRails = [];
-    for (let i = 0; i < this.rails; i++) {
-      myRails[i] = [];
-    }
-    return myRails;
+  getRailNumberForIndex (index) {
+    return this.railMapping[index % (this.railMapping.length)];
   }
 }
 
 export default RailFenceCipher;
 
+function range(start, stop, step) {
+  // return _.range(start, stop, step);
+  if (step === undefined) {
+    step = 1;
+  }
+  if (start > stop) return range(stop + 1, start + 1, step * -1).reverse();
+  const result = [];
+  for(let i = start; i < stop; i += step) {
+    result.push(i);
+  }
+  return result;
+}
+
+function initArrayOfArrays(size) {
+  return range(0, size).map(anyValue => []);
+}
+
+function flattenToString(myRails) {
+  return flatten(myRails).join('');
+}
+
+function flatten(myRails) {
+  return [].concat(...myRails);
+}
--- railFenceCipher.spec.js
+++ railFenceCipher.spec.js
@@ -112,19 +112,19 @@ describe('RailFenceCipher business class can', () => {
 
   describe('The order of rails to use', () => {
     test('Should make sense for 1', () => {
-      expect(new RailFenceCipher(1).generateRailToUse()).toEqual([0]);
+      expect(new RailFenceCipher(1).generateRailMapping()).toEqual([0]);
     });
 
     test('Should make sense for 2', () => {
-      expect(new RailFenceCipher(2).generateRailToUse()).toEqual([0, 1]);
+      expect(new RailFenceCipher(2).generateRailMapping()).toEqual([0, 1]);
     });
 
     test('Should make sense for 3', () => {
-      expect(new RailFenceCipher(3).generateRailToUse()).toEqual([0, 1, 2, 1]);
+      expect(new RailFenceCipher(3).generateRailMapping()).toEqual([0, 1, 2, 1]);
     });
 
     test('Should make sense for 4', () => {
-      expect(new RailFenceCipher(4).generateRailToUse()).toEqual([0, 1, 2, 3, 2, 1]);
+      expect(new RailFenceCipher(4).generateRailMapping()).toEqual([0, 1, 2, 3, 2, 1]);
     });
   });
 });
