addeac089677074333aa85758cecdef41c0e9bb2
David Braley <david.braley@gmail.com>
2017-12-06 12:55:06 -0500

    refactor

--- railFenceCipher.js
+++ railFenceCipher.js
@@ -32,12 +32,14 @@ class RailFenceCipher {
       }
       return input;
     }
-    if (input.length === 3) {
-      return input[0] + input[2] + input[1];
-    } else if (input.length === 4) {
-      return input[0] + input[2] + input[1] + input[3];
-    } else if (input === 'ACEBD') {
-      return 'ABCDE';
+    if (this.railCount === 2) {
+      if (input.length === 3) {
+        return input[0] + input[2] + input[1];
+      } else if (input.length === 4) {
+        return input[0] + input[2] + input[1] + input[3];
+      } else if (input.length === 5) {
+        return input[0] + input[3] + input[1] + input[4] + input[2];
+      }
     }
     return input;
   }
