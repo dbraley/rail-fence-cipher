f04443a895ae107220a5b10f004b62ed846a07c9
David Braley <david.braley@gmail.com>
2017-12-07 22:03:04 -0500

    refactor

--- railFenceCipher.js
+++ railFenceCipher.js
@@ -33,7 +33,6 @@ class RailFenceCipher {
         myRails[0].push(input[0]);
         myRails[1].push(input[1], input[2]);
         myRails[2].push(input[3]);
-        return myRails[0].shift() + myRails[1].shift() + myRails[2].shift() + myRails[1].shift();
       } else if (input.length === 5) {
         myRails[0].push(input[0], input[1]);
         myRails[1].push(input[2], input[3]);
