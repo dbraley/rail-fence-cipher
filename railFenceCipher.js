1d48c05e65a053f15045202f637ce74b10acb041
David Braley <david.braley@gmail.com>
2017-12-07 22:17:23 -0500

    refactor

--- railFenceCipher.js
+++ railFenceCipher.js
@@ -40,17 +40,35 @@ class RailFenceCipher {
         end = start + 2;
         myRails[2] = input.slice(start, end).split('');
       } else if (input.length === 5) {
-        myRails[0] = input.slice(0, 2).split('');
-        myRails[1] = input.slice(2, 4).split('');
-        myRails[2] = input.slice(4, 6).split('');
+        let start = 0;
+        let end = start + 2;
+        myRails[0] = input.slice(start, end).split('');
+        start = end;
+        end = start + 2;
+        myRails[1] = input.slice(start, end).split('');
+        start = end;
+        end = start + 2;
+        myRails[2] = input.slice(start, end).split('');
       } else if (input.length === 6) {
-        myRails[0] = input.slice(0, 2).split('');
-        myRails[1] = input.slice(2, 5).split('');
-        myRails[2] = input.slice(5, 7).split('');
+        let start = 0;
+        let end = start + 2;
+        myRails[0] = input.slice(start, end).split('');
+        start = end;
+        end = start + 3;
+        myRails[1] = input.slice(start, end).split('');
+        start = end;
+        end = start + 2;
+        myRails[2] = input.slice(start, end).split('');
       } else if (input.length === 7) {
-        myRails[0] = input.slice(0, 2).split('');
-        myRails[1] = input.slice(2, 5).split('');
-        myRails[2] = input.slice(5, 8).split('');
+        let start = 0;
+        let end = start + 2;
+        myRails[0] = input.slice(start, end).split('');
+        start = end;
+        end = start + 3;
+        myRails[1] = input.slice(start, end).split('');
+        start = end;
+        end = start + 3;
+        myRails[2] = input.slice(start, end).split('');
       }
       let result = '';
       for (let i = 0; i < input.length; i++) {
